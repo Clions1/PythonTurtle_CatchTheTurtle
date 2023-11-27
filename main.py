9cc998a042c95e0c4d03b7ace47b157bf49eeb92
Clions1 <ahmetyunussarikanjob@gmail.com>
2023-11-27 13:25:45 -0800

    turtle image added

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import turtle
 
+image="turtle1.gif"
+
 turtle_screen = turtle.Screen()
 turtle_screen.title("Catch the Turtle")
 
-theTurtle=turtle.Turtle
+theTurtle=turtle.Turtle()
+
+
+turtle_screen.addshape(image)
+theTurtle.shape(image)
+turtle.mainloop()
