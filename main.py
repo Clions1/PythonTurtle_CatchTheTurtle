6cfa45a20867f9a83fadfbfee3e9cd5f739841e6
Clions1 <ahmetyunussarikanjob@gmail.com>
2023-11-27 13:43:31 -0800

    turtle random position added

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import turtle
 
+from random import randint
 image="turtle1.gif"
 
 turtle_screen = turtle.Screen()
 turtle_screen.title("Catch the Turtle")
 
 theTurtle=turtle.Turtle()
-
+theTurtle.penup()
 
 turtle_screen.addshape(image)
 theTurtle.shape(image)
+while 1==1:
+    theTurtle.goto(randint(-300,0),randint(0,400))
+
+
+
+
+
+
+
+
 turtle.mainloop()
