e043e9aa5ff1ece08f760a6d0c4803ca927c72af
Clions1 <ahmetyunussarikanjob@gmail.com>
2023-11-28 08:56:16 -0800

    scoreboard added

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ import turtle
 from random import randint
 image="turtle1.gif"
 
+
+
+
+skorTablosu = turtle.Turtle()
+skorTablosu.penup()
+skorTablosu.hideturtle()
+skorTablosu.setposition(-290, 310)
+
+
+skor_metni = "Skor: 100"
+skorTablosu.write(skor_metni, False, align='left', font=('Arial', 14, 'normal'))
+
+
+
+
 turtle_screen = turtle.Screen()
 turtle_screen.title("Catch the Turtle")
 
